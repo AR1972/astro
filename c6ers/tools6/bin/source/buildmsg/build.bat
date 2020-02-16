90976d91f993ff0d63f57af4d7b2e524352b96a1
Adam <you@example.com>
2020-02-15 20:27:12 -0800

    build tools from source

--- c6ers/tools6/bin/source/buildmsg/build.bat
+++
@@ -1,6 +0,0 @@
-set ROOT=C:\astro
-set PATH=%ROOT%\c6ers\tools6\bin
-set LIB=%ROOT%\c6ers\tools6\lib;%ROOT%\c6ers\toolsvr\lib
-set INCLUDE=.;%ROOT%\c6ers\tools6\include;%ROOT%\c6ers\toolsvr\inc
-
-cl buildmsg.c -link /stack:8192
