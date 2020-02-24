e443289b5f8416f8f6094c4bc9e1dea5180e8038
Adam <you@example.com>
2020-02-23 18:06:57 -0800

    add dummy symbols file

--- lang/usa/dev/emm386/emmmes.asm
+++ lang/usa/dev/emm386/emmmes.asm
@@ -1,1 +1,171 @@
-; 
+PUBLIC ActiveMess
+PUBLIC Adj_Size
+PUBLIC Already_Inst
+PUBLIC Any_key
+PUBLIC AutoMess
+PUBLIC AvailHanMess
+PUBLIC AvailPagesMess
+PUBLIC bad_mc_state
+PUBLIC BadXMM
+PUBLIC DMAMODE_Len
+PUBLIC DMAMODE_Mess
+PUBLIC DMAMODE_Num
+PUBLIC DMASIZE_Len
+PUBLIC DMASIZE_Mess
+PUBLIC DMASIZE_Num
+PUBLIC DriverVersion
+PUBLIC EXCPE_Len
+PUBLIC EXCPE_Mess
+PUBLIC EXCPE_Num
+PUBLIC HMAonMsg
+PUBLIC InactiveMess
+PUBLIC Incorrect_DOS
+PUBLIC Incorrect_PRT
+PUBLIC InsfMem
+PUBLIC InstallMess
+PUBLIC InvMRA
+PUBLIC InvParm
+PUBLIC InvPFBA
+PUBLIC LIMVerMess
+PUBLIC MajVer
+PUBLIC MemSizeMess
+PUBLIC MinVer1
+PUBLIC MinVer2
+PUBLIC NoEMSmess
+PUBLIC No_PF_Avail
+PUBLIC NoWeitek
+PUBLIC NoXMM
+PUBLIC OF_aerr
+PUBLIC OF_amode
+PUBLIC OFFMess
+PUBLIC OF_inaccess
+PUBLIC OF_invparm
+PUBLIC OF_not_there
+PUBLIC OF_parmerr
+PUBLIC OF_proterr
+PUBLIC OF_rerr
+PUBLIC OF_rmode
+PUBLIC OF_verror
+PUBLIC OF_vmode
+PUBLIC OF_w_inaccess
+PUBLIC OF_w_not_inst
+PUBLIC OF_woff_err
+PUBLIC OF_woff_mode
+PUBLIC OF_won_mode
+PUBLIC OtherEMM
+PUBLIC OverlapWarn
+PUBLIC PEN_Len
+PUBLIC PEN_Mess
+PUBLIC PEN_Num
+PUBLIC PFBAMess
+PUBLIC PF_not_3_2
+PUBLIC PFWarning
+PUBLIC POE_Len
+PUBLIC POE_Mess
+PUBLIC POE_Num
+PUBLIC StatusMess
+PUBLIC TotalHanMess
+PUBLIC TotalPagesMess
+PUBLIC UMBavail
+PUBLIC UMBlargest
+PUBLIC UMBmemMsg
+PUBLIC UMBstart
+PUBLIC UMBstatusMess
+PUBLIC UserAbortMsg
+PUBLIC WeitekNAMess
+PUBLIC WeitekOFFMess
+PUBLIC WeitekONMess
+PUBLIC WinBackfillMess
+PUBLIC WINEMM_Mess
+PUBLIC WinInvPathMess
+PUBLIC Inv_DOS_msg 
+PUBLIC InitMessage
+PUBLIC ISizeMess
+PUBLIC OF_won_err
+
+;##############################################################################
+
+ActiveMess
+Adj_Size
+Already_Inst
+Any_key
+AutoMess
+AvailHanMess
+AvailPagesMess
+bad_mc_state
+BadXMM
+DMAMODE_Len
+DMAMODE_Mess
+DMAMODE_Num
+DMASIZE_Len
+DMASIZE_Mess
+DMASIZE_Num
+DriverVersion
+EXCPE_Len
+EXCPE_Mess
+EXCPE_Num
+HMAonMsg
+InactiveMess
+Incorrect_DOS
+Incorrect_PRT
+InsfMem
+InstallMess
+InvMRA
+InvParm
+InvPFBA
+LIMVerMess
+MajVer
+MemSizeMess
+MinVer1
+MinVer2
+NoEMSmess
+No_PF_Avail
+NoWeitek
+NoXMM
+OF_aerr
+OF_amode
+OFFMess
+OF_inaccess
+OF_invparm
+OF_not_there
+OF_parmerr
+OF_proterr
+OF_rerr
+OF_rmode
+OF_verror
+OF_vmode
+OF_w_inaccess
+OF_w_not_inst
+OF_woff_err
+OF_woff_mode
+OF_won_mode
+OtherEMM
+OverlapWarn
+PEN_Len
+PEN_Mess
+PEN_Num
+PFBAMess
+PF_not_3_2
+PFWarning
+POE_Len
+POE_Mess
+POE_Num
+StatusMess
+TotalHanMess
+TotalPagesMess
+UMBavail
+UMBlargest
+UMBmemMsg
+UMBstart
+UMBstatusMess
+UserAbortMsg
+WeitekNAMess
+WeitekOFFMess
+WeitekONMess
+WinBackfillMess db 'WinBackfillMess'
+WINEMM_Mess
+WinInvPathMess
+Inv_DOS_msg 
+InitMessage
+ISizeMess
+OF_won_err
