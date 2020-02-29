53447e66b0282491855cd7229b7e52472effbc19
Adam <you@example.com>
2020-02-29 13:52:12 -0800

    add strings for ROMDRIVE.SYS

--- lang/usa/dev/romdrive/romdmsg.asm
+++ lang/usa/dev/romdrive/romdmsg.asm
@@ -1,1 +1,6 @@
-;
+PUBLIC HEADERMES
+PUBLIC DOS_DRV
+PUBLIC BADVERMES
+HEADERMES     db 13,10,"Microsoft ROMDrive version ????"
+DOS_DRV       db "A:",13,10,'$'
+BADVERMES     db "ROMDrive: Incorrect DOS version",13,10,'$'
