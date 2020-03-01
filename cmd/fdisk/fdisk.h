d3fe9e2696b52489c3bac75cc06796e29210bafb
Adam <you@example.com>
2020-02-29 21:21:12 -0800

    add some defines to fdisk.h

--- cmd/fdisk/fdisk.h
+++ cmd/fdisk/fdisk.h
@@ -8,12 +8,25 @@
 #define ESC 0x1B
 #define TRUE 1
 #define FALSE 0
+#define DOS12 0x01
+#define DOS16 0x06
+#define DOS16_SIZE 16 // ?
 #define FAT12 0x01
 #define FAT16 0x06
-#define DOSNEW 0x0B
+#define FAT16_SIZE 16 // ?
+#define DOSNEW 0x0B // FAT32 ???
 #define EXTENDED 0x05
 #define BAD_BLOCK 0xFF
 #define BYTES_PER_SECTOR 512
+#define SEA 0x43 // "C"
+#define INSTALLATION_CHECK 0 // ?
+#define NETWORK 0 // ?
+#define SERVER_CHECK 0 // ?
+#define NOVOLUME 0 // ?
+#define NOFORMAT 0 // ?
+#define NETWORK_IOCTL 0 // ?
+#define ERR_LEVEL_0 0 // ?
+#define ERR_LEVEL_1 1 // ?
 
 struct entry
 {
