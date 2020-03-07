8ffed6e9a8a5e2e44c1b517c3890c622337a12b8
Adam <you@example.com>
2020-03-07 11:18:26 -0800

    add strings for FASTHELP.EXE, add to build

---
+++ lang/usa/cmd/help/message.h
@@ -0,0 +1,6 @@
+char usage[] = "Provides help information for MS-DOS commands.\n\nFASTHELP [command]\n\n  command - displays help information on that command.\n";
+char file_not_found[] = "Help file could not be found.\n";
+char read_error[] = "Error reading help file.\n";
+char general_help[] = "\nFor more information on a specific command, type FASTHELP command-name.\n";
+char more_prompt[] = "---More---";
+char help_not_available[] = "Help not available for this command.\n";
