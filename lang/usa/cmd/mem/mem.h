5b45256c416a11534c7f63617ece9f1a825dcbab
Adam <you@example.com>
2020-03-11 19:23:14 -0700

    add strings for MEM.EXE

--- lang/usa/cmd/mem/mem.h
+++ lang/usa/cmd/mem/mem.h
@@ -1,31 +1,35 @@
 #include "stdarg.h"
-#define MAX_DDRIVER_REP 3
-#define GET_UMB_LINK_STATE 0x5802
-#define SET_UMB_LINK_STATE 0x5803
-#define UNLINK_UMBS 0x0000
-#define LINK_UMBS 0x0001
-#define FALSE	 (char)(1==0)
-#define TRUE	 !(FALSE)
-#define CR	 '\x0d'
-#define LF	 '\x0a'
-#define NUL	 (char) '\0'
-#define TAB	 '\x09'
-#define BLANK	' '
-#define	MAX_CLDATA_INDEX	100
-/* defines used by total memory determination */
-#define GET_PSP 	(unsigned char ) 0x62		 /* get PSP function call */
-#define MEMORY_DET	0x12		/* BIOS interrupt used to get total memory size */
-#define CASSETTE 0x15 // interupt 15
-#define EMS 0x67
-#define GET_VECT 0x35
-#define DOSEMSVER 0x40
-#define GetExtended 0x8800
-#define EMSGetVer 0x4600
-#define EMSGetFreePgs 0x4200
-#define EMSGetStat 0x4000
-#define toK(x) ((x)>>10)
+#define MAX_DDRIVER_REP      0x19
+#define GET_UMB_LINK_STATE   0x5802
+#define SET_UMB_LINK_STATE   0x5803
+#define UNLINK_UMBS          0x0000
+#define LINK_UMBS            0x0001
+#define FALSE                (char)(1==0)
+#define TRUE                 !(FALSE)
+#define CR                   '\x0d'
+#define LF                   '\x0a'
+#define NUL                  (char) '\0'
+#define TAB                  '\x09'
+#define BLANK                ' '
+#define	MAX_CLDATA_INDEX     100
+#define GET_PSP              (unsigned char ) 0x62 /* get PSP function call */
+#define MEMORY_DET           0x12                  /* BIOS interrupt used to get total memory size */
+#define CASSETTE             0x15                  /* interupt 15 */
+#define EMS                  0x67
+#define GET_VECT             0x35
+#define DOSEMSVER            0x40
+#define GetExtended          0x8800
+#define EMSGetVer            0x4600
+#define EMSGetFreePgs        0x4200
+#define EMSGetStat           0x4000
+#define toK(x)               ((x)>>10)
+#define p_too_many           1
+#define p_op_missing         2
+#define p_not_in_sw          3
+#define p_not_in_key         4
 #undef IBMCOPYRIGHT
-static const char SumFormat[] = "%5lx%3ld%8ld%7c";
+
+static const char SumFormat[] = "%-16m%8ld%8c%8ld%8c%8ld%8c";
 static const char MemFormat[] = "%-16m%6c%6c%6c";
 
 struct files
@@ -33,7 +37,7 @@ struct files
     unsigned char psp_addr;
     long conv_ttl;
     long umb_ttl;
-	char driveridx;
+    char driveridx;
 };
 
 struct umbs
@@ -50,88 +54,81 @@ struct mem_classif
     unsigned long conv_free;
     unsigned long umb_ttl;
     unsigned long umb_free;
-	unsigned long umb_large;
+    unsigned long umb_large;
     unsigned long xms_ttl;
     unsigned long xms_free;
     unsigned long ems_ttl;
-	unsigned long ems_free;
+    unsigned long ems_free;
     unsigned long int_15h;
-	unsigned long conv_large;
+    unsigned long conv_large;
     int hma;
-	unsigned long rom_ttl;
+    unsigned long rom_ttl;
     char noof_progs;
-	char noof_umbs;
+    char noof_umbs;
     char xmsMvers;
     char xmsmvers;
     char xmsMdrvr;
     char xmsmdrvr;
     char emsMvers;
-	char emsmvers;
-	struct files files[MAX_CLDATA_INDEX];
-	struct umbs umbs[MAX_CLDATA_INDEX];
+    char emsmvers;
+    struct files files[MAX_CLDATA_INDEX];
+    struct umbs umbs[MAX_CLDATA_INDEX];
 };
 
 struct	DEVICEHEADER {
-	struct DEVICEHEADER far *NextDeviceHeader;
-	unsigned		Attributes;
-	unsigned		Strategy;
-	unsigned		Interrupt;
-	char			Name[8];
+    struct DEVICEHEADER far *NextDeviceHeader;
+    unsigned    Attributes;
+    unsigned    Strategy;
+    unsigned    Interrupt;
+    char        Name[8];
 	};
 	
 struct	SYSIVAR {
-	char far *DpbChain;
-	char far *SftChain;
-	char far *Clock;
-	char far *Con;
-	unsigned  MaxSectorSize;
-	char far *BufferChain;
-	char far *CdsList;
-	char far *FcbChain;
-	unsigned  FcbKeepCount;
-	unsigned char BlockDeviceCount;
-	char	  CdsCount;
-	struct DEVICEHEADER far *DeviceDriverChain;
-	unsigned  NullDeviceAttributes;
-	unsigned  NullDeviceStrategyEntryPoint;
-	unsigned  NullDeviceInterruptEntryPoint;
-	char	  NullDeviceName[8];
-	char	  SpliceIndicator;
-	unsigned  DosParagraphs;
-	char far *DosServiceRntryPoint;
-	char far *IfsChain;
-	unsigned  BufferValues;
-	unsigned  LastDriveValue;
-	char	  BootDrive;
-	char	  MoveType;
-	unsigned  ExtendedMemory;
+    char far *DpbChain;
+    char far *SftChain;
+    char far *Clock;
+    char far *Con;
+    unsigned  MaxSectorSize;
+    char far *BufferChain;
+    char far *CdsList;
+    char far *FcbChain;
+    unsigned  FcbKeepCount;
+    unsigned char BlockDeviceCount;
+    char      CdsCount;
+    struct DEVICEHEADER far *DeviceDriverChain;
+    unsigned  NullDeviceAttributes;
+    unsigned  NullDeviceStrategyEntryPoint;
+    unsigned  NullDeviceInterruptEntryPoint;
+    char      NullDeviceName[8];
+    char      SpliceIndicator;
+    unsigned  DosParagraphs;
+    char far *DosServiceRntryPoint;
+    char far *IfsChain;
+    unsigned  BufferValues;
+    unsigned  LastDriveValue;
+    char      BootDrive;
+    char      MoveType;
+    unsigned  ExtendedMemory;
 	};
 
 struct sublistx {
-	 unsigned char size;	       /* sublist size			       */
-	 unsigned char reserved;       /* reserved for future growth	       */
-	 unsigned far *value;	       /* pointer to replaceable parm	       */
-	 unsigned char id;	       /* type of replaceable parm	       */
-	 unsigned char flags;	       /* how parm is to be displayed	       */
+	 unsigned char size;	       /* sublist size                         */
+	 unsigned char reserved;       /* reserved for future growth           */
+	 unsigned far *value;	       /* pointer to replaceable parm          */
+	 unsigned char id;             /* type of replaceable parm             */
+	 unsigned char flags;	       /* how parm is to be displayed          */
 	 unsigned char max_width;      /* max width of replaceable field       */
 	 unsigned char min_width;      /* min width of replaceable field       */
 	 unsigned char pad_char;       /* pad character for replaceable field  */
 	};
 
 struct	ARENA	 {
-	char	 Signature;
+	char     Signature;
 	unsigned Owner;
 	unsigned Paragraphs;
-	char	 Dummy[3];
-	char	 OwnerName[8];
+	char     Dummy[3];
+	char     OwnerName[8];
 	};
-
-enum {
-	p_not_in_key,
-	p_too_many,
-	p_not_in_sw,
-	p_op_missing
-};
 
 ///////////////////////////////////////////////////////////////////////////////
 
