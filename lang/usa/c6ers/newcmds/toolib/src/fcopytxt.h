978485a6628eb3f5a158a3bed22de445f9441bff
Adam <you@example.com>
2020-03-19 21:52:11 -0700

    git add error messages

--- lang/usa/c6ers/newcmds/toolib/src/fcopytxt.h
+++ lang/usa/c6ers/newcmds/toolib/src/fcopytxt.h
@@ -1,13 +1,12 @@
-#define NOT_ENOUGH_MEM 1
-#define CANT_OPEN_SOURCE 2
-#define CANT_CREATE_DEST 3
-#define CANT_WRITE_DEST 4
-#define CANT_READ_SOURCE 5
-#define CANT_GET_SRC_TIME 6
-#define CANT_SET_DEST_TIME 7
-#define CANT_GET_SRC_ATTR 8
-#define CANT_SET_DEST_ATTR 9
-
 char szErrUnableToDelete[] = "Unable to delete destination";
-char stardotstar[] = "*.*";
 char pszMsgRunChkDsk[] = "\nDELTREE has found an error in your directory structure. Run Chkdsk /F to repair.\n";
+
+char NOT_ENOUGH_MEM[] = "not enough memory";
+char CANT_OPEN_SOURCE[] = "Unable to open source";
+char CANT_CREATE_DEST[]= "Unable to create destination";
+char CANT_WRITE_DEST[] = "Unable to write destination";
+char CANT_READ_SOURCE[] = "Unable to read source";
+char CANT_GET_SRC_TIME[] = "Unable to get source modification time";
+char CANT_SET_DEST_TIME[] = "Unable to set destination modification time";
+char CANT_GET_SRC_ATTR[] = "Unable to get source attributes";
+char CANT_SET_DEST_ATTR[] = "Unable to set destination attributes";
