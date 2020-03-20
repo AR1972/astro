2d8bdd55bf15f3a25e67db7b98dbc24d82a7b101
Adam <you@example.com>
2020-03-19 18:37:19 -0700

    fix build of appcomp libs

--- c6ers/appcomp/makeit.bat
+++ c6ers/appcomp/makeit.bat
@@ -1,7 +1,5 @@
 @echo off
-rem set PATHSAV=%PATH%
-rem set INCSAV=%INCLUDE%
-rem set LIBSAV=%LIB%
+
 set DEBUGSAV=%DEBUG%
 
 set CMPROOT=
@@ -12,55 +10,17 @@ set OS=DOS
 set OSLET=R
 set MODEL=S
 
-goto :got_env
-
-rem
-rem Default to building with C6
-rem
-rem goto :c51_env
-
-rem C6 build
-
-set CVER=C6
-set PATH=c:\astro\c6ers\tools6\bin;c:\bin
-set INCLUDE=c:\astro\c6ers\tools6\include;%CMPROOT%
-set LIB=c:\astro\c6ers\tools6\lib;%CMPROOT%
-
-goto :got_env
-
-rem C5.1 build
-
-:c51_env
-set CVER=C51
-set PATH=c:\astro\tools\bin;c:\bin
-set INCLUDE=c:\astro\tools\include;%CMPROOT%
-set LIB=c:\astro\tools\lib;%CMPROOT%
-
-:got_env
-
-nmake -f mkfile2 -nologo clean > NUL
-nmake -f mkfile2 -nologo		 > MAKE.OUT
-
+nmake -f mkfile2 -nologo
 copy sdecompr.lib ..\..\lib
-rem copy compress.exe %RELEASE%\compress.exe
-rem copy decomp.exe %RELEASE%\decomp.exe
 
 set DEBUG=NO
-set MODEL=L
+set MODEL=M
 
-nmake -f mkfile2 -nologo clean > NUL
-nmake -f mkfile2 -nologo		 >> MAKE.OUT
-
+nmake -f mkfile2 -nologo
 copy ldecompr.lib ..\..\lib
 
-rem set PATH=%PATHSAV%
-rem set INCLUDE=%INCSAV%
-rem set LIB=%LIBSAV%
 set DEBUG=%DEBUGSAV%
 
-rem set PATHSAV=
-rem set INCSAV=
-rem set LIBSAV=
 set DEBUGSAV=
 set DECOMP=
 set OS=
